fe4787817c8d8930961c5bb4abd6d5b6f1c28779
hexu <hexu@163.com>
2020-01-02 18:02:21 +0800

    163

--- springcloud-zuul-gateway9527/src/main/java/com/hx/CloudProviderDeptApplication.java
+++ springcloud-zuul-gateway9527/src/main/java/com/hx/CloudProviderDeptApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+//测试git163
 @EnableEurekaClient//将服务注册到eureka注册中心
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableZuulProxy
